68c789e4a4b7beb2eee0d2224854ae2ceacd9d28
Theodore Cleveland <tcclevela@gmail.com>
2013-07-12 18:04:45 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeanFiend::Application.config.secret_token = 'c7251d23d4999b9428c738d22f15755f04cb3f4dd611d90ed0d04d9704bd513d1de89a2d4ba424d6a8f967832dbdb4ec787f462f053414dd7a362b1137b57c7e'
