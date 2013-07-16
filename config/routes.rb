adf8bec8e797567258eada3b490facad383fa89a
Theodore Cleveland <tcclevela@gmail.com>
2013-07-15 23:12:12 -0400

    added devise authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 BeanFiend::Application.routes.draw do
 
+  devise_for :users
+
   root :to => "welcome#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
