fedaec2f6afb4701e6eab01ac6ccf3670eddf95b
Theodore Cleveland <tcclevela@gmail.com>
2013-07-15 23:50:25 -0400

    setup review model

---
+++ app/models/review.rb
@@ -0,0 +1,4 @@
+class Review < ActiveRecord::Base
+	belongs_to :store
+	belongs_to :user
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ BeanFiend::Application.routes.draw do
   devise_for :users
 
   root :to => "welcome#index"
-  resources :locations
+  resources :stores
+  resources :reviews
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
