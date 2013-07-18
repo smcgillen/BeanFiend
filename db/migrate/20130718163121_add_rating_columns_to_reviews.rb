4cada6f2f8a11d69c400015a47afc3178d54a04e
Theodore Cleveland <tcclevela@gmail.com>
2013-07-18 17:02:00 -0400

    changed limit on integer ratings from 10 to 10.0 for heroku migration

--- db/migrate/20130718163121_add_rating_columns_to_reviews.rb
+++ db/migrate/20130718163121_add_rating_columns_to_reviews.rb
@@ -1,9 +1,9 @@
 class AddRatingColumnsToReviews < ActiveRecord::Migration
   def change
-  	add_column :reviews, :noise, :integer, default: 5, limit: 10
- 	add_column :reviews, :people, :integer, default: 5, limit: 10
- 	add_column :reviews, :wifi, :integer, default: 5, limit: 10
- 	add_column :reviews, :price, :integer, default: 5, limit: 10
- 	add_column :reviews, :coffee, :integer, default: 5, limit: 10
+  	add_column :reviews, :noise, :integer, default: 5, limit: 10.0
+ 	add_column :reviews, :people, :integer, default: 5, limit: 10.0
+ 	add_column :reviews, :wifi, :integer, default: 5, limit: 10.0
+ 	add_column :reviews, :price, :integer, default: 5, limit: 10.0
+ 	add_column :reviews, :coffee, :integer, default: 5, limit: 10.0
   end
 end
